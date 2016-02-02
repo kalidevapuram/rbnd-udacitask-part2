40a671e493615ceecbab42c7edc72c63bb01569b
Bhargavi <kalidevapurambhargavi@gmail.com>
2016-02-02 00:02:48 -0800

    Created an untitled list and add items, also add formate_date and format_priority methods into module listable

--- lib/listable.rb
+++ lib/listable.rb
@@ -6,7 +6,27 @@ module Listable
   	"#{description}".ljust(30)
   end
   
+  def format_date(*date)
   
+if (date.length == 1 ) || (date.length == 0 )
+		@due ? @due.strftime("%D") : "No due date"
+		else
+  		dates = @start_date.strftime("%D") if @start_date
+    	dates << " -- " + @end_date.strftime("%D") if @end_date
+    	dates = "N/A" if !dates
+    	return dates
+	    
+	end
+	end
+	
+	
+	def format_priority(priority)
+		value = " ⇧".colorize(:color => :green)if @priority == "high"
+    	value = " ⇨".colorize(:color => :yellow) if @priority == "medium"
+    	value = " ⇩".colorize(:color => :red) if @priority == "low"
+    	value = " " if !@priority
+    	return value
+    end
   
   
   
--- lib/todo.rb
+++ lib/todo.rb
@@ -7,21 +7,11 @@ class TodoItem
     @due = options[:due] ? Chronic.parse(options[:due]) : options[:due]
     @priority = options[:priority]
   end
-  def format_date
-  		@due ? @due.strftime("%D") : "No due date"
-
-  end
-  def format_priority
-  value = " ⇧".colorize(:color => :green)if @priority == "high"
-    	value = " ⇨".colorize(:color => :yellow) if @priority == "medium"
-    	value = " ⇩".colorize(:color => :red) if @priority == "low"
-    	value = " " if !@priority
-    	return value
-  end
+ 
   
   def details
     format_description(@description) + "due: " +
-    format_date + 
-    format_priority
+    format_date(@due) + 
+    format_priority(@priority)
   end
 end
